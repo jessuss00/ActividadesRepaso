be24acb235ab5aa2c4cb01784b1faab0c96279ba
jessuss00 <jesusmadro23@gmail.com>
2024-10-29 13:09:51 +0100

    nue

---
+++ tema2/src/tema2/examen.java
@@ -0,0 +1,23 @@
+package tema2;
+
+public class examen {
+
+	public static void main(String[] args) {
+		int numero =2;
+		switch (numero) {
+		case 1:
+			System.out.println("Hoy");
+		case 2:
+			System.out.println("Hay");
+		case 3:
+			System.out.println("examen");
+		case 4:
+			System.out.println("Porgramacion");
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
